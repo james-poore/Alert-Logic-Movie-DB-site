c57c9de5ae4ec9c05c0f30941d071a92e8b19d00
James Poore <james.poore.jr@gmail.com>
2015-06-07 04:43:47 -0500

    fixed navbar to top and added stubs for movie and person info pages

--- site.rb
+++ site.rb
@@ -15,8 +15,9 @@ set :environment, :development
 # TMDB Configuration
 tmdb_api_key_file = File.open(File.join(File.dirname(__FILE__), "config/tmdb_api_key"), mode = "r")
 Tmdb::Api.key(tmdb_api_key_file.readline)
-
 Tmdb::Api.language("en")
+
+configuration = Tmdb::Configuration.new
 
 
 #Pages
@@ -28,17 +29,29 @@ get '/' do
 	haml :home, :locals => {:now_playing => @now_playing}
 end
 
+
+
 get '/movie/:movie_id' do |movie_id|
 
   movie = Tmdb::Movie.detail(movie_id)
+  cast = Tmdb::Movie.casts(movie_id)
+  similar_movies = Tmdb::Movie.similar_movies(movie_id)
+  trailers = Tmdb::Movie.trailers(movie_id)
+  images = Tmdb::Movie.images(movie_id)['backdrops']
 
+  haml :movie_info, :locals => {:movie => movie}
 end
+
+
 
 get '/person/:person_id' do |person_id|
 
   person = Tmdb::Person.detail(person_id)
 
+  haml :person_info, :locals => {:person_info => person_info}
 end
+
+
 
 get '/movies' do
 
@@ -47,12 +60,16 @@ get '/movies' do
   haml :movies, :locals => {:popular_movies => @popular_movies}
 end
 
+
+
 get '/people' do
 
   @popular_people = Tmdb::Person.popular
 
   haml :people, :locals => {:popular_people => @popular_people}
 end
+
+
 
 post '/search' do
 
@@ -62,5 +79,5 @@ post '/search' do
   @movies = Tmdb::Movie.find(search_string)
   @people = Tmdb::Person.find(search_string)
 
-	haml :search, :locals => {:movies => @movies, :people => @people}
+	haml :search_results, :locals => {:movies => @movies, :people => @people}
 end
